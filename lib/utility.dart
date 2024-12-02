209b899281eb61361adf95f6329f5c84abc818a3
Sagar-at-079925877420 <sagar.s@girdmail.com>
2024-12-02 15:49:31 +0530

    added widgets

--- lib/utility.dart
+++ lib/utility.dart
@@ -3,10 +3,6 @@ library utility;
 export 'package:get/get.dart';
 export 'package:utility/shared_pref_service/SharedService.dart';
 export 'package:utility/widgets/txt_field.dart';
-
-export 'package:get/get.dart';
-export 'package:utility/shared_pref_service/SharedService.dart';
-export 'package:utility/widgets/txt_field.dart';
 export 'package:lottie/lottie.dart';
 export 'package:utility/widgets/success_bottom_sheet.dart';
 export 'package:utility/widgets/error_bottom_sheet.dart';
